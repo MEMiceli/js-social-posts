ea298786d20f35aba5dfc4aab6f3d1c7ce54f9c4
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-05-16 16:41:48 +0200

    array  o non array ecco il dilemma

--- main.js
+++ main.js
@@ -55,3 +55,62 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+// Descrizione
+// Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+// Milestone 1 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+// Milestone 2 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+// Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+// BONUS
+// 1. Formattare le date in formato italiano (gg/mm/aaaa)
+// 2. Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
+// 3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
+// Buon lavoro
+
+// commentare template di prova e prendere elemento contenitore da html
+
+let container = document.querySelector(".posts-list");
+
+for(let i = 0; i < posts.length; i++){
+    const post =`
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${posts[i].author.image}" alt="${posts[i].author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${posts[i].author.name}</div>
+                        <div class="post-meta__time">${posts[i].created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${posts[i].content}</div>
+            <div class="post__image">
+                <img src="${posts[i].media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+        `;
+        if(posts[i].author.image === null){
+            
+            posts[i].author.image = posts[i].author.name;
+        }
+    container.innerHTML += post;    
+}
+//prendere i post dall'oggetto in js tramite ciclo for
+
+// aggiungere evento click per aggiungere caratteristica da css con cambio colore e modifica numero like post collegato
+
+//pushare id con evento click in un secondo array
